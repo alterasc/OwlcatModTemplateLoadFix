1106d78775bb4e6219cdae58e107e7f37b24870e
alterasc <alterasc@protonmail.com>
2022-10-01 16:07:56 +0200

    init

---
+++ ModTemplateLoadFix.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Kingmaker.BundlesLoading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OwlcatModTemplateLoadFix.Base
+{
+    public static class ModTemplateLoadFix
+    {
+        [HarmonyPatch(typeof(LocationList), nameof(LocationList.OnAfterDeserialize))]
+        private class LocationList_OnAfterDeserialize_Patch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                if (codes.Count == 30)
+                {
+                    codes.RemoveRange(codes.Count - 7, 6);
+                }
+                return codes.AsEnumerable();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+﻿# Owlcat Mod Template Load Fix
+
+Fixes error when loading mods that use Owlcat Mod Template (Homebrew Archetypes, Great Radiance, Camelia Romance Expanded, etc )
+
+This mod can break completely as soon as Owlcat changes something, so if you have any problems, remove it immediately when troubleshooting.   
+
+Remove this mod after every patch to check if it's needed. It has a flimsy safeguard to try to deactivate itself if Owlcat changes things, but it's far from reliable.
+
+Install as any other UMM mod.
+
+Thanks to Kurufinve on instructions what to fix.
